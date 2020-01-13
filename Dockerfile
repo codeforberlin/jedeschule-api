d7b64394a2b5880c492eb53bf8faf6cb3a786982
Knut Hühne <knut@k-nut.eu>
2020-01-13 11:45:19 +0100

    Use pg_isready for waiting for postgres

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ ADD ./requirements.txt /requirements.txt
 
 RUN pip install -r /requirements.txt
 
+RUN apt-get update
+RUN apt-get install postgresql-client-11 --yes
+
 COPY ./app /app/app
 COPY ./prestart.sh /app/prestart.sh
 COPY ./test_connection.py /app
--- prestart.sh
+++ prestart.sh
@@ -1,8 +1,8 @@
 echo "Waiting for postgres..."
 
-# TODO: Use pg_isready instead? This is probably not installed here though...
-while ! python test_connection.py; do
-  sleep 0.5
+until pg_isready -h db -p 5432
+do
+  sleep 2.5
 done
 
 echo "PostgreSQL started"
