a0f542701ccd6ab0951ee09ef0b6cd217c87b275
Knut Hühne <knut@k-nut.eu>
2021-02-14 21:29:07 +0100

    Clean up

--- app/crud.py
+++ app/crud.py
@@ -6,8 +6,11 @@ from . import models
 from .schemas import School
 
 
-def get_school(db: Session, school_id: str):
-    return db.query(models.School).filter(models.School.id == school_id).first()
+def get_school(db: Session, school_id: str) -> School:
+    return db\
+        .query(models.School)\
+        .filter(models.School.id == school_id) \
+        .first()
 
 
 def get_schools(db: Session, skip: int = 0, limit: int = 100, filter_params=None) -> List[School]:
@@ -17,7 +20,7 @@ def get_schools(db: Session, skip: int = 0, limit: int = 100, filter_params=None
     return query.offset(skip).limit(limit).all()
 
 
-def get_stats(db: Session, skip: int = 0, limit: int = 100):
+def get_stats(db: Session):
     response = db.execute("""select substring(id, 1, 2) as state, count(*) as count
 from schools
 group by state
