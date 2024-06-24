60661d41da0e18b3b8405e15b2af96f285c05460
Knut Hühne <knut@k-nut.eu>
2024-06-24 09:35:23 +0200

    Update SQLAlchemy and related dependencies

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 engine = create_engine(os.environ.get("DATABASE_URL"))
